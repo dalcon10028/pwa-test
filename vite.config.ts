1724162be1eb1c7f034171f799796ceacd8d44dd
corn <dalcon10280@gmail.com>
2023-04-22 00:25:22 +0900

    2

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react(), VitePWA({
     injectRegister: 'auto',
-    includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+    // includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
     manifest: {
       name: 'My Awesome App',
       short_name: 'MyApp',
@@ -14,12 +14,12 @@ export default defineConfig({
       theme_color: '#ffffff',
       icons: [
         {
-          src: 'pwa-192x192.png',
+          src: 'assets/icons/icon-256.png',
           sizes: '192x192',
           type: 'image/png'
         },
         {
-          src: 'pwa-512x512.png',
+          src: 'assets/icons/icon-256.png',
           sizes: '512x512',
           type: 'image/png'
         }
